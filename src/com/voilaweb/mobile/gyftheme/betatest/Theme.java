6df70e0128fc0c604600afd998a07a7501050db4
Chris F Ravenscroft <github@voilaweb.com>
2013-01-27 00:32:26 -0800

    First check-in

---
+++ src/com/voilaweb/mobile/gyftheme/betatest/Theme.java
@@ -0,0 +1,10 @@
+package com.voilaweb.mobile.gyftheme.betatest;
+
+import android.app.Activity;
+
+/**
+ * Author: Chris F. Ravenscroft
+ * Date: 1/24/13
+ */
+public class Theme extends Activity {
+}
